a944b5fac6fb3edbfa4b8807263c4df026f918bb
samaa99 <samaasoltan6@gmail.com>
2025-10-07 13:51:28 +0300

    first commit

--- ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ContentView.swift
@@ -8,12 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var players: [Player] = [
+        Player(name: "Omar", score: 0, color: .orange),
+        Player(name: "Ali", score: 0, color: .teal),
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Score Keeper")
+                .font(.title)
+                .bold()
+            
+            List {
+                Grid {
+                    GridRow {
+                        Text("Player")
+                            .font(.headline)
+                        
+                        Text("Score")
+                            .font(.headline)
+                        
+                    }
+                    .gridColumnAlignment(.leading)
+                    .padding(.top)
+                    
+                    ForEach($players) { $player in
+                        GridRow {
+                            TextField("Name", text: $player.name)
+                                .autocorrectionDisabled(true)
+                            
+                            Text("\(player.score)")
+                                .padding(.trailing, 8)
+                                .foregroundStyle(player.color)
+                            
+                            Stepper("\(player.score)", value: $player.score, in: 0...20)
+                                .labelsHidden()
+                                
+                        }
+                    }
+                }
+                
+                Button("Add Player", systemImage: "plus") {
+                    players.append(Player(name: "", score: 0, color: .clear))
+                }
+                
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            
+            HStack {
+                Spacer()
+                
+                Button("Edit") {
+                     
+                 }
+                .buttonStyle(.borderedProminent)
+                .font(.headline)
+                
+                Spacer()
+
+            }
+        
         }
         .padding()
     }
---
+++ ScoreKeeper/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  ScoreKeeper
+//
+//  Created by Samaa Soltan on 07/10/2025.
+//
+
+import Foundation
+import SwiftUICore
+
+
+struct Player: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    var score: Int = 0
+    var color: Color
+}
